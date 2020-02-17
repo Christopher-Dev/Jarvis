994439051e7c90a279d7799332bac60fffd7d817
Christopher-Dev <59783738+Christopher-Dev@users.noreply.github.com>
2020-02-17 03:59:20 -0500

    Modifed and cleaned up code

--- Body.py
+++ Body.py
@@ -1,15 +1,16 @@
 import discord
 import random
 from discord.ext import commands
+import Commands
 
 TOKEN = ''
-client = commands.Bot(command_prefix = '$')#command for bot is the tilda ( $ )
+client = commands.Bot(command_prefix = '$')#command for bot is the tilda ( @ )
 client.remove_command('help')
 
 @client.event
 #prints bot is ready in terminal
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('Who am i?'))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('How can i help you Sir?'))
     print('Jarvis is Online')
 #below command is used for information about the bot.
 @client.command()
@@ -18,13 +19,13 @@ async def help(ctx):
     embed.set_footer(text='this is a footer.')
     embed.set_image(url='https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fec%2F06%2F04%2Fec06042c52a6bd554950ffd86a55b7a0.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F131730357821798335%2F&tbnid=oqqsTHwNEl5htM&vet=12ahUKEwiXs8SooM3nAhXTlVMKHVEqAjsQMygAegUIARDbAQ..i&docid=bFbuwTGNvwUNHM&w=185&h=210&q=Chris%20knight%20real%20genius%20images&safe=active&ved=2ahUKEwiXs8SooM3nAhXTlVMKHVEqAjsQMygAegUIARDbAQ')
     embed.set_thumbnail(url='https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fec%2F06%2F04%2Fec06042c52a6bd554950ffd86a55b7a0.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F131730357821798335%2F&tbnid=oqqsTHwNEl5htM&vet=12ahUKEwiXs8SooM3nAhXTlVMKHVEqAjsQMygAegUIARDbAQ..i&docid=bFbuwTGNvwUNHM&w=185&h=210&q=Chris%20knight%20real%20genius%20images&safe=active&ved=2ahUKEwiXs8SooM3nAhXTlVMKHVEqAjsQMygAegUIARDbAQ')
-    embed.set_author(name='Edwin')
+    embed.set_author(name='Jarvis')
     icon_url='https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fec%2F06%2F04%2Fec06042c52a6bd554950ffd86a55b7a0.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F131730357821798335%2F&tbnid=oqqsTHwNEl5htM&vet=12ahUKEwiXs8SooM3nAhXTlVMKHVEqAjsQMygAegUIARDbAQ..i&docid=bFbuwTGNvwUNHM&w=185&h=210&q=Chris%20knight%20real%20genius%20images&safe=active&ved=2ahUKEwiXs8SooM3nAhXTlVMKHVEqAjsQMygAegUIARDbAQ'
-    embed.add_field(name=".clear", value="Will clear the default of 25 messages. Enter a number for a specific amount of messages.", inline=True)
-    embed.add_field(name=".Whitelisting", value="Opens the Squad White-listing application.", inline=True)
+    embed.add_field(name="@clear", value="Will clear the default of 25 messages. Enter a number for a specific amount of messages.", inline=True)
+    embed.add_field(name="@Whitelisting", value="Opens the Squad White-listing application.", inline=True)
     embed.set_footer(text="This Bot is made by Edwin and allowed only for use by Users that have been verified by the creator.")
-    embed.add_field(name=".ping", value="Returns Pong! and Server latency.", inline=False)
-    embed.add_field(name=".help", value="Prints out the Server Bot commands.", inline=True)
+    embed.add_field(name="@ping", value="Returns Pong! and Server latency.", inline=False)
+    embed.add_field(name="@help", value="Prints out the Server Bot commands.", inline=True)
     await ctx.send(embed=embed)
 
 @client.command()
@@ -38,9 +39,10 @@ async def HouseParty(ctx):
     #Edwin#4323, Hwydrifter#8173, Daddysnow#7904
   
 @client.command()
-async def killedwin(ctx):
+async def killedwin(ctx, amount=2):
     embed=discord.Embed(title="You can't kill me! I'm everywhere!", color=0xff0000)
     embed.set_footer(text="This Message will self destruct in 2 mins.")
+    await ctx.channel.purge(limit=amount)
     await ctx.send(embed=embed)
 #only prints when member has joined or left in terminal
 @client.event
@@ -56,7 +58,7 @@ async def on_member_remove(member):
 @client.command()
 async def clear(ctx, amount=26):
     await ctx.channel.purge(limit=amount)
-    embed=discord.Embed(title="I have cleared the last 25 messages for you sir", color=0x00ff00)
+    embed=discord.Embed(title="I have cleared upto the last 25 messages for you sir", color=0x00ff00)
     embed.set_thumbnail(url="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSEg1KbS2iNOL_jYT5XHZ8161fx5AeAzgah-eHmkCNnhGuJYDVi")
     await ctx.send(embed=embed)
 
@@ -68,8 +70,10 @@ async def ping(ctx):
 async def kick(ctx, member : discord.Member, *, reason=None):
     await member.kick(reason=reason)
 
+
+
 @client.command()
-async def send_dm(ctx, member: discord.Member, *, content):
+async def welcome(ctx, member: discord.Member, *, content):
     embed=discord.Embed(title="Welcome to the Jarvis Dev Server", description="Enter $help in the Jarvis channel for info on the commands.", color=0x00ff00)
     embed.set_thumbnail(url="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSEg1KbS2iNOL_jYT5XHZ8161fx5AeAzgah-eHmkCNnhGuJYDVi")
     await ctx.send(embed=embed)
